e99449ccf0964f3d2de6f2cc77a25560b4d52215
Gabriela <gabriela.l.campi@outlook.com>
2019-12-26 20:22:51 -0300

    init commit

---
+++ src/main/java/com/farol/SaoJudas/SaoJudasApplication.java
@@ -0,0 +1,13 @@
+package com.farol.SaoJudas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoJudasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoJudasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farol/SaoJudas/SaoJudasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farol.SaoJudas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoJudasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
